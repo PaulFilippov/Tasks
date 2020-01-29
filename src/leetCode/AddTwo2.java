f76596fb6552a87dc6042b8c81dffdd9d1f43524
Paul Filippov <fpspro@inbox.ru>
2020-01-29 10:44:04 +0300

    some ex from leetcode

---
+++ src/leetCode/AddTwo2.java
@@ -0,0 +1,53 @@
+package leetCode;
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+
+public class AddTwo2 {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        int sum = 0, carry=0;
+        while (l1!=null || l2!=null)
+    }
+
+
+    public static void main(String[] args) {
+
+        ListNode l11 = new ListNode(2);
+        ListNode l12 = new ListNode(4);
+        l11.next = l12;
+        ListNode l13 = new ListNode(6);
+        l12.next = l13;
+
+        ListNode l21 = new ListNode(2);
+        ListNode l22 = new ListNode(4);
+        l21.next = l22;
+        ListNode l23 = new ListNode(6);
+        l22.next = l23;
+        ListNode l24 = new ListNode(6);
+        l23.next = l24;
+
+
+
+
+    }
+
+}
