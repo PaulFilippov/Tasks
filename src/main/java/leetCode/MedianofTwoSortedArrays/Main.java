b76ff47cdb5fb7f0cabe237f19eff944ce09f5e1
pavel <filippovpaul@bk.ru>
2021-02-11 11:39:46 +0300

    leetcode: 4.Median of Two Sorted Arrays

---
+++ src/main/java/leetCode/MedianofTwoSortedArrays/Main.java
@@ -0,0 +1,10 @@
+package leetCode.MedianofTwoSortedArrays;
+
+public class Main {
+    public static void main(String[] args) {
+    int mas1[] = {1,2};
+    int mas2[] = {3,4};
+    Solution sol = new Solution();
+    System.out.println(sol.findMedianSortedArrays(mas1, mas2));
+    }
+}
---
+++ src/main/java/leetCode/MedianofTwoSortedArrays/Solution.java
@@ -0,0 +1,43 @@
+package leetCode.MedianofTwoSortedArrays;
+
+public class Solution {
+    int k = 0;
+    int i = 0;
+    int j = 0;
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int lenthResultMas = nums1.length + nums2.length;
+        int mergeMas[] = new int[lenthResultMas];
+        while (i < nums1.length & j < nums2.length) {
+            if (nums1[i] <= nums2[j]) {
+                mergeMas[k] = nums1[i];
+                ++i;
+            } else {
+                mergeMas[k] = nums2[j];
+                ++j;
+            }
+            ++k;
+        }
+        while (i < nums1.length) {
+            mergeMas[k] = nums1[i];
+            ++k;
+            ++i;
+        }
+        while (j < nums2.length) {
+            mergeMas[k] = nums2[j];
+            ++k;
+            ++j;
+        }
+        return mediane(mergeMas, mergeMas.length);
+    }
+
+    public double mediane(int mas[], int length) {
+        double med = 0;
+        if (length % 2 == 0) {
+            med = (double) (mas[length/2] + mas[(length/2) - 1]) / (double) 2;
+        } else {
+            med = mas[length / 2];
+        }
+        return med;
+    }
+}
